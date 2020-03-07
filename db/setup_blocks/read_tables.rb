1bea9f038b242b0770a6c13a39d77cc640ed0055
jakethesnnake <jakedwillson@gmail.com>
2020-03-06 19:39:42 -0600

    (3/6) copied db files into local db folder

--- db/setup_blocks/read_tables.rb
+++ db/setup_blocks/read_tables.rb
@@ -1,66 +1,66 @@
 module ReadTables
   def read_full_citation_list
-    CSV.read("lib/csv-tables/full-citation-list.csv", :headers => true).each { |row| Citation.create!(row.to_h) }
+    CSV.read("db/csv-tables/full-citation-list.csv", :headers => true).each { |row| Citation.create!(row.to_h) }
   end
 
   def read_all
-    CSV.read("lib/csv-tables/table1.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table2.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table3.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table4.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table5.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table6.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table7.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table9.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table10.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table11.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table12.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table13-to-17.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table19.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table21.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table23.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table24.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table28.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table29.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table30.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table32.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table33.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
-    CSV.read("lib/csv-tables/table34-to-37.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table1.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table2.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table3.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table4.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table5.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table6.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table7.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table9.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table10.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table11.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table12.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table13-to-17.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table19.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table21.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table23.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table24.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table28.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table29.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table30.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table32.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table33.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
+    CSV.read("db/csv-tables/table34-to-37.csv", :headers => true).each { |row| Weight.create!(row.to_h) }
   end
 
   def read_all_references
-    CSV.read("lib/csv-tables/citations/table1.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table2.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table3.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table4.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table5.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table6.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table7.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table9.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table10.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table11.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table12.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table13.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table16.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table17.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table19.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table21.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table23.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table24.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table28.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table29-33.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table34.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table35.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table36.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table37.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table1.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table2.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table3.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table4.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table5.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table6.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table7.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table9.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table10.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table11.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table12.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table13.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table16.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table17.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table19.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table21.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table23.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table24.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table28.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table29-33.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table34.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table35.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table36.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table37.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
   end
 
   def read_table_only_citations
-    CSV.read("lib/csv-tables/citations/table_only/table4.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table_only/table5.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table_only/table13-17.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table_only/table29-33.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
-    CSV.read("lib/csv-tables/citations/table_only/table34-37.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table_only/table4.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table_only/table5.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table_only/table13-17.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table_only/table29-33.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
+    CSV.read("db/csv-tables/citations/table_only/table34-37.csv", :headers => true).each { |row| TableCitation.add!(row.to_h) }
   end
 
   def create_table_objects
